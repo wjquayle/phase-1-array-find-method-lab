bf29b173bae197dd0e37c9fc148f2c5821ced742
Bill <wjquayle@gmail.com>
2023-08-07 21:09:38 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+
+]
+
+function superbowlWin(record) {
+    const win = record.find(entry => entry.result === "W");
+    return win ? win.year : undefined;
+  }
